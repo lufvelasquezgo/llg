8e25a6cd8b55615dc8b6a29f24d6b1ab82e2e407
Juan David <jdalzatec@unal.edu.co>
2019-09-10 21:13:07 -0500

    Remove some unnecessary variables

--- tests/ffunctions/external_fields/test_thermal_field.py
+++ tests/ffunctions/external_fields/test_thermal_field.py
@@ -84,43 +84,40 @@ def test_thermal_field_ones_matrix(num_sites):
 
 @pytest.mark.repeat(100)
 def test_thermal_field_random_matrix(num_sites, random_directions):
-    mat = random_directions
     assert numpy.allclose(
         external_fields.thermal_field(
-            mat, [1] * num_sites,
-            [1] * num_sites, 1.0, 1.0, 1.0, 1.0), mat * numpy.sqrt(2))
+            random_directions, [1] * num_sites,
+            [1] * num_sites, 1.0, 1.0, 1.0, 1.0), random_directions * numpy.sqrt(2))
 
 
 @pytest.mark.repeat(100)
 def test_thermal_field_random_temperature(num_sites, random_temperature):
-    tem = random_temperature
-    values = numpy.transpose(numpy.ones((3, num_sites)) * numpy.sqrt(2 * tem))
+    values = numpy.transpose(numpy.ones((3, num_sites))
+                             * numpy.sqrt(2 * random_temperature))
 
     assert numpy.allclose(
         external_fields.thermal_field(
-            numpy.ones((num_sites, 3)), tem,
+            numpy.ones((num_sites, 3)), random_temperature,
             [1] * num_sites, 1.0, 1.0, 1.0, 1.0), values)
 
 
 @pytest.mark.repeat(100)
 def test_thermal_field_random_magnitude_spin(num_sites, random_magnitude_spin):
-    rms = random_magnitude_spin
-    values = numpy.transpose(numpy.ones((3, num_sites)) * numpy.sqrt(2 / rms))
+    values = numpy.transpose(numpy.ones((3, num_sites))
+                             * numpy.sqrt(2 / random_magnitude_spin))
 
     assert numpy.allclose(
         external_fields.thermal_field(
             numpy.ones((num_sites, 3)), [1] * num_sites,
-            rms, 1.0, 1.0, 1.0, 1.0), values)
+            random_magnitude_spin, 1.0, 1.0, 1.0, 1.0), values)
 
 
 @pytest.mark.repeat(100)
 def test_thermal_field_random_temperature_random_magnitude_spin(num_sites, random_temperature, random_magnitude_spin):
-    tem = random_temperature
-    rms = random_magnitude_spin
     values = numpy.transpose(numpy.ones((3, num_sites))
-                             * numpy.sqrt(2 * tem / rms))
+                             * numpy.sqrt(2 * random_temperature / random_magnitude_spin))
 
     assert numpy.allclose(
         external_fields.thermal_field(
-            numpy.ones((num_sites, 3)), tem,
-            rms, 1.0, 1.0, 1.0, 1.0), values)
+            numpy.ones((num_sites, 3)), random_temperature,
+            random_magnitude_spin, 1.0, 1.0, 1.0, 1.0), values)
