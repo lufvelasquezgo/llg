9478fff309336a8a74a55b9a3a54caf407e6218d
Juan David <jdalzatec@unal.edu.co>
2019-09-15 22:30:36 -0500

    Add test for null magnetic moments

--- tests/ffunctions/spin_fields/test_exchange_interaction_field.py
+++ tests/ffunctions/spin_fields/test_exchange_interaction_field.py
@@ -48,21 +48,32 @@ def test_exchange_interaction_field_random_J_exchange(random_state_spins, build_
 
 
 @pytest.mark.repeat(100)
-def test_exchange_interaction_field_random_spin_moments(random_state_spins, build_sample, random_spin_moments):
-    if numpy.any(random_spin_moments == 0.0):
-        raise ValueError("Magnetic moment must be non-zero")
-
-    num_sites, num_interactions, neighbors, num_neighbors = build_sample
-    j_exchange = numpy.full(num_interactions, numpy.random.uniform(-10, 10))
+def test_exchange_interaction_field_random_spin_moments(random_state_spins, build_sample, random_spin_moments, random_j_exchange):
+    num_sites, _, neighbors, num_neighbors = build_sample
     total = compute_exchange_field(
-        num_sites, random_state_spins, j_exchange, random_spin_moments, num_neighbors, neighbors)
+        num_sites, random_state_spins, random_j_exchange, random_spin_moments, num_neighbors, neighbors)
     assert numpy.allclose(spin_fields.exchange_interaction_field(
-        random_state_spins, random_spin_moments, j_exchange, num_neighbors, neighbors), total)
+        random_state_spins, random_spin_moments, random_j_exchange, num_neighbors, neighbors), total)
+
+
+@pytest.mark.repeat(100)
+def test_exchange_interaction_field_null_magnetic_moments(random_state_spins, build_sample, random_j_exchange):
+    num_sites, _, neighbors, num_neighbors = build_sample
+    null_moments = [0.0] * num_sites
+    total = numpy.full((num_sites, 3), numpy.inf)
+    assert(numpy.allclose(
+        numpy.abs(
+            spin_fields.exchange_interaction_field(
+                random_state_spins, null_moments, random_j_exchange, num_neighbors, neighbors
+            )
+        ),
+        total
+    ))
 
 
 @pytest.mark.repeat(100)
 def test_exchange_interaction_field_null_interactions(random_state_spins, build_sample):
-    num_sites, num_interactions, _, _ = build_sample
+    num_sites, _, _, _ = build_sample
     spin_moments = numpy.ones(shape=num_sites)
     j_exchange = []
     num_neighbors = numpy.zeros(shape=num_sites, dtype=int)
