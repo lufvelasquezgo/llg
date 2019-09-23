d903ba0b0ccbd686b2b8e6f31fb6414b430a2c7e
Daniel Felipe Rodríguez Patiño <dfrodriguezp@unal.edu.co>
2019-09-23 09:59:15 -0500

    Update external_fields.py

--- src/llg/functions/external_fields.py
+++ src/llg/functions/external_fields.py
@@ -1,7 +1,14 @@
+"""
+Module for the computation of the external fields of the LLG equation.
+These external fields are the thermal field and the magnetic field.
+"""
 import numpy
 
 
-def thermal_field(random_normal_matrix, temperature, magnitude_spin_moment, damping, deltat, gyromagnetic, kB):
+def thermal_field(random_normal_matrix, temperature, magnitude_spin_moment, damping,
+                  deltat, gyromagnetic, kB):
+    """Function that returns the thermal field on each site"""
+    
     random_normal_matrix = numpy.array(random_normal_matrix)
     num_sites = len(random_normal_matrix)
     out = numpy.zeros((num_sites, 3))
@@ -13,6 +20,7 @@ def thermal_field(random_normal_matrix, temperature, magnitude_spin_moment, damp
 
 
 def magnetic_field(intensities, directions):
+    """Function that returns the magnetic field on each site"""
     intensities = numpy.array(intensities)
     directions = numpy.array(directions)
     num_sites = len(intensities)
