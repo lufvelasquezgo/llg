2b4b03f5748fec3a446ece0ed6a4b2a5429b7ce6
Daniel Felipe Rodríguez Patiño <dfrodriguezp@unal.edu.co>
2019-10-04 17:33:53 -0500

    Implement first version for reading input files

--- src/llg/cli.py
+++ src/llg/cli.py
@@ -3,14 +3,79 @@
 """Console script for llg."""
 import sys
 import click
+import yaml
+import numpy
+from matplotlib import pyplot, style
+from tqdm import tqdm
+from llg.llg import read_sample_file
+from llg.ffunctions import heun, mag_functions
+style.use("classic")
 
 
 @click.command()
-def main(args=None):
+@click.argument("file")
+def main(file, args=None):
     """Console script for llg."""
-    click.echo("Replace this message by putting your code into "
-               "llg.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    with open(file, "r") as parameters:
+        try:
+            data = yaml.safe_load(parameters)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    sample_file = data.get("sample")
+    anisotropy_file = data.get("anisotropy", None)
+    num_iterations = data.get("num_iterations", 1)
+    damping = data.get("damping", 1.0)
+    gyromagnetic = data.get("gyromagnetic", 1.76e11)
+    deltat = data.get("deltat", 1e-15)
+    H = data.get("field", 0)
+    T = data.get("temperature", 10)
+    seed = data.get("seed", 696969)
+    units = data.get("units", "mev")
+    skipping = data.get("skipping", 1)
+
+    if units == "mev":
+        kb = 0.08618
+    elif units == "joules":
+        kb = 1.38064852e-23
+    else:
+        kb = 1.0
+
+    sample_info = read_sample_file(sample_file)
+    num_sites = sample_info["num_sites"]
+    num_interactions = sample_info["num_interactions"]
+    state = sample_info["initial_state"]
+    spin_moment = sample_info["spin_norm"]
+    temperature = [T] * num_sites
+    field_int = [H] * num_sites
+    field_dir = sample_info["field_dir"]
+    j_exchange = sample_info["j_exchange"]
+    num_neighbors = sample_info["num_neighbors"]
+    neighbors = sample_info["neighbors"]
+
+    if not anisotropy_file:
+        anis_const = [0.0] * num_sites
+        anis_vect = [[0, 0, 1]] * num_sites
+
+    magnetization_arr = []
+    for i in tqdm(range(num_iterations)):
+        matrix = numpy.random.normal(size=(num_sites, 3))
+        state = heun.integrate(state, spin_moment, matrix,
+                               temperature, damping, deltat, gyromagnetic, kb,
+                               field_int, field_dir, j_exchange, num_neighbors,
+                               neighbors, anis_const, anis_vect)
+        mag = mag_functions.magnetization_vector(state)[2]
+        magnetization_arr.append(mag)
+
+    pyplot.figure()
+    pyplot.plot(magnetization_arr)
+    pyplot.ylim(-1, 1)
+    pyplot.savefig("magnetization.pdf")
+    pyplot.close()
+
+    # click.echo("Replace this message by putting your code into "
+    #            "llg.cli.main")
+    # click.echo("See click documentation at http://click.pocoo.org/")
     return 0
 
 
--- src/llg/llg.py
+++ src/llg/llg.py
@@ -1,4 +1,60 @@
 # -*- coding: utf-8 -*-
-
 """Main module."""
-print("hola mundo")
+
+from collections import defaultdict
+import numpy
+
+
+def read_sample_file(file):
+    indexes = []
+    position = []
+    spin_norm = []
+    field_dir = []
+    types = []
+    initial_state = []
+    j_exchange = []
+    num_neighbors = []
+    neighbors = []
+
+    with open(file, "r") as input_file:
+        lines = input_file.readlines()
+        num_sites, num_interactions, num_types = list(
+            map(int, lines[0].split(" ")))
+        for i in range(1, num_types + 1):
+            types.append(lines[i].replace("\n", ""))
+
+        for i in range(num_types + 1, num_types + num_sites + 1):
+            line = lines[i].split(" ")
+            indexes.append(int(line[0]))
+            position.append([float(line[1]), float(line[2]), float(line[3])])
+            spin_norm.append(float(line[4]))
+            field_dir.append([float(line[5]), float(line[6]), float(line[7])])
+            types.append(str(line[8]))
+            initial_state.append(
+                [float(line[9]), float(line[10]), float(line[11])])
+
+        dict_neighbors = defaultdict(list)
+        for i in range(num_types + num_sites + 1, num_types + num_sites + num_interactions + 1):
+            line = lines[i].split(" ")
+            dict_neighbors[line[0]].append(line[1])
+            neighbors.append(float(line[1]))
+            j_exchange.append(float(line[2]))
+
+    for site in dict_neighbors.keys():
+        num_neighbors.append(len(dict_neighbors[site]))
+
+    output = {
+        "num_sites": num_sites,
+        "num_interactions": num_interactions,
+        "num_types": num_types,
+        "indexes": numpy.array(indexes),
+        "position": numpy.array(position),
+        "spin_norm": numpy.array(spin_norm),
+        "field_dir": numpy.array(field_dir),
+        "types": numpy.array(types),
+        "initial_state": numpy.array(initial_state),
+        "j_exchange": numpy.array(j_exchange),
+        "num_neighbors": numpy.array(num_neighbors),
+        "neighbors": numpy.array(neighbors)
+    }
+    return output
