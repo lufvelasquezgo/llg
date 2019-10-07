319195365f71ef4570eb93d22742c608ef2e6a20
Juan David <jdalzatec@unal.edu.co>
2019-10-06 23:07:01 -0500

    Unsupport python 3.5

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
