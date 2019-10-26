2ee456ad25c7365cdac8e82e3326e3465e5bd90e
Juan David <jdalzatec@unal.edu.co>
2019-10-26 15:41:46 -0500

    Correct exchange_energy and compute zeeman energy

--- fortran-src/energy.f90
+++ fortran-src/energy.f90
@@ -26,11 +26,14 @@ contains
             do j = start, end
                 nhb = neighbors(j)
                 exchange_energy(i, :) = exchange_energy(i, :) - &
-                j_exchange(i) * dot_product(state(i, :), state(nhb, :)) 
+                    j_exchange(i) * dot_product(state(i, :), state(nhb, :)) 
             end do
         end do
 
+        exchange_energy = 0.5 * exchange_energy
+
     end function exchange_energy
+
 
     function anisotropy_energy(num_sites, state, &
         anisotropy_constant, anisotropy_vector)
@@ -47,10 +50,30 @@ contains
         anisotropy_energy = 0.d0
         do i = 0, num_sites - 1
             anisotropy_energy(i, :) = anisotropy_energy(i, :) - &
-            anisotropy_constant(i) * &
-            (dot_product(state(i, :), anisotropy_vector(i, :))) ** 2
+                anisotropy_constant(i) * &
+                dot_product(state(i, :), anisotropy_vector(i, :)) ** 2
         end do
         
     end function anisotropy_energy
+
+
+    function magnetic_field(num_sites, magnitude_spin_moment, state, intensities, directions)
+        implicit none
+        integer, intent(in) :: num_sites
+        real*8, dimension(0:(num_sites - 1)) :: magnitude_spin_moment
+        real*8, intent(in), dimension(0:(num_sites - 1), 0:2) :: state
+        real*8, intent(in), dimension(0:(num_sites - 1)) :: intensities
+        real*8, intent(in), dimension(0:(num_sites - 1), 0:2) :: directions
+        real*8, dimension(0:(num_sites - 1), 0:2) :: magnetic_field
+        integer :: i
+
+        magnetic_field = 0.d0
+        do i = 0, num_sites - 1
+            magnetic_field(i, :) = magnetic_field(i, :) - &
+                magnitude_spin_moment(i) * &
+                dot_product(state(i, :), directions(i, :)) * intensities(i)
+        end do
+        
+    end function magnetic_field
 
 end module Energy
