3bb5fad53ee7827899fcd29a012d0001eabff1f8
Luisa Fernanda <lufvelasquezgo@unal.edu.co>
2019-11-06 21:00:59 -0500

    Store energies in hdf5

--- src/llg/cli.py
+++ src/llg/cli.py
@@ -17,8 +17,15 @@ def main():
 def simulate(configuration_file):
     simulation = Simulation.from_file(configuration_file)
     print(pickle.dumps(simulation.information))
-    for state, _, _, _, _ in simulation.run():
+    for values in simulation.run():
+        state, exchange_energy, anisotropy_energy, magnetic_energy, total_energy = (
+            values
+        )
         print(pickle.dumps(state))
+        print(pickle.dumps(exchange_energy))
+        print(pickle.dumps(anisotropy_energy))
+        print(pickle.dumps(magnetic_energy))
+        print(pickle.dumps(total_energy))
 
 
 @main.command("store-hdf")
@@ -37,5 +44,16 @@ def store_hdf_cli(output):
 
         for i in range(num_TH):
             for j in range(num_iterations):
+                # reads
                 state = pickle.loads(eval(input()))
+                exchange_energy = pickle.loads(eval(input()))
+                anisotropy_energy = pickle.loads(eval(input()))
+                magnetic_energy = pickle.loads(eval(input()))
+                total_energy = pickle.loads(eval(input()))
+
+                # stores
                 hdf5_file.store_state(state, i, j)
+                hdf5_file.store_exchange_energy(exchange_energy, i, j)
+                hdf5_file.store_anisotropy_energy(anisotropy_energy, i, j)
+                hdf5_file.store_magnetic_energy(magnetic_energy, i, j)
+                hdf5_file.store_total_energy(total_energy, i, j)
