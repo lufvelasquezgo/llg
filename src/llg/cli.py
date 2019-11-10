1442f665633f04cb32d9a307e6c80b82c66d9a3a
Juan David <jdalzatec@unal.edu.co>
2019-11-10 17:52:35 -0500

    Implement CLI to read hdf files

--- src/llg/cli.py
+++ src/llg/cli.py
@@ -4,6 +4,7 @@
 import click
 from llg import Simulation, StoreHDF
 import pickle
+import h5py
 
 
 @click.group()
@@ -18,9 +19,13 @@ def simulate(configuration_file):
     simulation = Simulation.from_file(configuration_file)
     print(pickle.dumps(simulation.information))
     for values in simulation.run():
-        state, exchange_energy, anisotropy_energy, magnetic_energy, total_energy = (
-            values
-        )
+        (
+            state,
+            exchange_energy,
+            anisotropy_energy,
+            magnetic_energy,
+            total_energy,
+        ) = values
         print(pickle.dumps(state))
         print(pickle.dumps(exchange_energy))
         print(pickle.dumps(anisotropy_energy))
@@ -57,3 +62,36 @@ def store_hdf_cli(output):
                 hdf5_file.store_anisotropy_energy(anisotropy_energy, i, j)
                 hdf5_file.store_magnetic_energy(magnetic_energy, i, j)
                 hdf5_file.store_total_energy(total_energy, i, j)
+
+
+@main.command("read-hdf")
+@click.argument("file")
+def read_hdf(file):
+    with h5py.File(file, mode="r") as dataset:
+        simulation_information = {
+            "num_sites": dataset.attrs["num_sites"],
+            "parameters": {
+                "units": dataset.attrs["units"],
+                "damping": dataset.attrs["damping"],
+                "gyromagnetic": dataset.attrs["gyromagnetic"],
+                "deltat": dataset.attrs["deltat"],
+                "kb": dataset.attrs["deltat"],
+            },
+            "temperature": dataset["temperature"][:],
+            "field": dataset["field"][:],
+            "seed": dataset.attrs["seed"],
+            "num_iterations": dataset.attrs["num_iterations"],
+            "positions": dataset["positions"][:],
+            "types": dataset["types"][:],
+            "initial_state": dataset["initial_state"][:],
+            "num_TH": dataset.attrs["num_TH"],
+        }
+        print(pickle.dumps(simulation_information))
+
+        for i in range(dataset.attrs["num_TH"]):
+            for j in range(dataset.attrs["num_iterations"]):
+                print(pickle.dumps(dataset["states"][i, j]))
+                print(pickle.dumps(dataset["exchange_energy"][i, j]))
+                print(pickle.dumps(dataset["anisotropy_energy"][i, j]))
+                print(pickle.dumps(dataset["magnetic_energy"][i, j]))
+                print(pickle.dumps(dataset["total_energy"][i, j]))
