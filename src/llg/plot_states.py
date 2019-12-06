c5a54abf3d8f2852b9bd72edba1844fd768b2784
Luisa Fernanda <lufvelasquezgo@unal.edu.co>
2019-12-06 11:29:35 -0500

    Create a class to plot the states

--- src/llg/plot_states.py
+++ src/llg/plot_states.py
@@ -2,6 +2,30 @@ import vapory
 import numpy
 
 
-class PlotStates:
-    def __init__(self, output):
-        self.output = output
+def Arrow(center, direction, color):
+    center = numpy.array(center)
+    direction = numpy.array(direction) * 0.9
+    base_point_cylinder = center - 0.5 * direction
+    cap_point_cone = center + 0.5 * direction
+    cap_point_cylinder = base_point_cone = base_point_cylinder + 0.5 * direction
+
+    radius_cylinder = 1 / 20
+    base_radius_cone = 1 / 6
+
+    cylinder = vapory.Cylinder(
+        base_point_cylinder,
+        cap_point_cylinder,
+        radius_cylinder,
+        vapory.Texture(vapory.Pigment("color", color)),
+    )
+
+    cone = vapory.Cone(
+        base_point_cone,
+        base_radius_cone,
+        cap_point_cone,
+        0.0,
+        vapory.Texture(vapory.Pigment("color", color)),
+    )
+
+    return vapory.Union(cone, cylinder)
+
