9ad9d96f1d9c02c4bb0c04be6f16cc37a8d8a9f2
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2020-04-10 21:18:53 -0500

    Compute magnetization and magnetization by type

---
+++ src/llg/functions/magnetization.py
@@ -0,0 +1,28 @@
+import numpy
+from nptyping import NDArray
+from typing import Any
+
+
+def magnetization_vector(state: NDArray[(Any, 3), float],) -> NDArray[(Any, 3), float]:
+    return state.mean(axis=1)
+
+
+def magnetization_vector_by_type(
+    state: NDArray[(Any, 3), float], num_types: int, types: NDArray[Any, float],
+) -> NDArray[(Any, Any, 3), float]:
+    out = numpy.zeros(shape=(num_types, 3))
+    for t in range(num_types):
+        mask = types == t
+        out[t] = state[mask].mean(axis=1)
+    return out
+
+
+def total_magnetization(state: NDArray[(Any, 3), float],) -> float:
+    return numpy.linalg.norm(magnetization_vector(state))
+
+
+def magnetization_by_type(
+    state: NDArray[(Any, 3), float], num_types: int, types: NDArray[Any, float],
+) -> NDArray[Any, float]:
+    mag = magnetization_vector_by_type(state, num_types, types)
+    return numpy.linalg.norm(mag, axis=1)
